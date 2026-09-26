030382f36b600efd91fb75171f916f3c1497626e
km6307 <krishnamurarim752@gmail.com>
2026-09-26 15:55:40 +0530

    try some thing new

--- FeedbackSystem/feedbackapp/models.py
+++ FeedbackSystem/feedbackapp/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 def create_student_users():
     """Create the default student accounts when explicitly called."""
-    users_to_create = []
+    users = []
     for i in range(1, 71):
-        student_id = f"MP/CS/{i}"
-        user = User(username=student_id)
+        student_id = f"MP_CS_{i}"
+        user = User(student_id=student_id)
         user.set_password(f"MP@{i}")
-        users_to_create.append(user)
+        users.append(user)
 
-    User.objects.bulk_create(users_to_create, ignore_conflicts=True)
-    return len(users_to_create)
+    User.objects.bulk_create(users, ignore_conflicts=True)
+    return len(users)
